5e16ee6c3aa544649b80cf0f02dab03b7a75878c
nathkn <41593086+nathkn@users.noreply.github.com>
2018-08-01 13:45:28 -0500

    Restructuring for Angular 6

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,16 @@
 FROM node:8-alpine as builder
 
-# Create app directory
 RUN mkdir -p /app
 WORKDIR /app
-
-# Bundle app source
 COPY . /app
 
 RUN npm install 
 RUN npm install -g @angular/cli@6.1.1
+
 RUN ng build --prod
 
 FROM node:8-alpine
+
 RUN mkdir -p /app/server /app/dist
 WORKDIR /app
 
--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -1,6 +1,11 @@
 import { AppComponent } from './app.component';
+import { BurrowService } from '../services/burrow.service';
+import { ConsumerService } from '../services/consumer.service';
+import { HomeService } from '../services/home.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -11,7 +16,9 @@ describe('AppComponent', function () {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      declarations: [ AppComponent ],
+      imports: [ RouterTestingModule, HttpClientModule ],
+      providers: [ ConsumerService, BurrowService, HomeService ]
     })
     .compileComponents();
   }));
@@ -25,7 +32,7 @@ describe('AppComponent', function () {
   it('should create component', () => expect(comp).toBeDefined() );
 
   it('should have expected <h1> text', () => {
-    fixture.detectChanges();
+    fixture.autoDetectChanges();
     const h1 = de.nativeElement;
     expect(h1.innerText).toMatch(/angular/i,
       '<h1> should say something about "Angular"');
